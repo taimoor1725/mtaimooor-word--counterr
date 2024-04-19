fc2d3b44c89d0a6b452a7afd019d982f8d91a5f5
taimoor1725 <159219973+taimoor1725@users.noreply.github.com>
2024-04-19 11:43:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// impport "inquirer" module, which is a command line interfce for Node.js
+import inquirer from "inquirer";
+// Declare a constant "answer" and assign it to the result of inquirer.promt function
+const answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+// split is liye use krty hen k jitny bh apky pass elements hongy unko humen array 
+// ki form men convert karne hen take hum words ko console per dekh saken
+const words = answer.Sentence.trim().split(" ");
+// print the arrow of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
